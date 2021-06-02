0f89518e60acfc398420a5b3a81f74319b534795
Philip Ehrens <phil@archive.org>
2021-06-02 16:08:13 +0000

    Fixed admin static files location - Actually an Apache config item. See upload/apache2/conf-available/DPS-cgi.conf

--- vault_site/settings.py
+++ vault_site/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+#SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+with open('/etc/_django_secret_key_vault_') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'phil-dev.us.archive.org', '207.241.225.89']
 
 
 # Application definition
@@ -131,10 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/vault/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "vault/static",
+    BASE_DIR / 'vault/static/',
 ]
+
+#STATIC_ROOT = BASE_DIR / 'vault/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
