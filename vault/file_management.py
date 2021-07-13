7bbd18b81b96d9d8f82143dc3372df885c35ad3f
Tabish Shaikh <tabish@archive.org>
2021-07-13 15:55:20 +0530

    capturing errors using sentry sdk

--- vault/file_management.py
+++ vault/file_management.py
@@ -4,6 +4,7 @@ import logging
 import unicodedata
 import re
 from vault import models
+from sentry_sdk import capture_exception, capture_message
 
 logger = logging.getLogger(__name__)
 
@@ -90,9 +91,11 @@ def db_file_update(fname, bakname):
         pass
     except models.File.MultipleObjectsReturned:
         err = f"db_file_update: multiple db rows matched {fname}"
+        capture_message(err)
         logger.error(err)
         raise DBFileUpdateError(err)
     except Exception as e:
+        capture_exception(e)
         logger.error(f"While handling {bakname} : {e}")
 
 
