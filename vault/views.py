7d208f83953908228cc12da81dc8e3ce31a7c660
Alex Dempsey <avdempsey@archive.org>
2021-09-16 15:30:23 -0700

    render_file_view uses Organization.tree_node
    It's safer and faster to look up the Organization root TreeNode using the new foreign key.

--- vault/views.py
+++ vault/views.py
@@ -1,27 +1,21 @@
 import datetime
-import os
-import re
-import logging
 import json
-
+import logging
 from functools import reduce
 
-# :FIXME: - Required to make Apache use Django auth
-# from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.db import transaction
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import MultipleObjectsReturned
 from django.db.models import Max, Sum, Count
-from django.db.models.fields import NullBooleanField
 from django.http import Http404
+from django.http import HttpResponse
 from django.shortcuts import redirect, get_object_or_404
 from django.template.response import TemplateResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from vault import forms
 from vault import models
 from vault.file_management import generateHashes, move_temp_file
-from django.http import HttpResponse
-
-from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
 
@@ -457,32 +451,32 @@ def deposit_flow(request):
     )
 
 
-import time
-
-
 @login_required
 def render_file_view(request, path):
-    start = time.perf_counter()
+    org = request.user.organization
     output_path = ""
     if not path:
-        path = request.user.organization.name
+        path = org.name
     else:
-        path = request.user.organization.name + "/" + path
-    parent = get_object_or_404(models.TreeNode, name=request.user.organization.name)
-    print(parent.name)
-    spilt_path = path.split("/")[1:]
-    child = NullBooleanField
-    print(spilt_path)
-    for node in spilt_path:
+        path = f"{org.name}/{path}"
+    parent = org.tree_node
+    split_path = path.split("/")[1:]
+    for node in split_path:
         try:
+            # this lookup assumes there is only one child node with a given name
             child = get_object_or_404(models.TreeNode, name=node, parent=parent)
-            print(child)
-            output_path += "/" + child.name
+            output_path = f"{output_path}/{child.name}"
             parent = child
-        except:
-            return TemplateResponse(request, "vault/files_view.html", {"status": 404})
-    children = parent.get_descendants()
-    end = time.perf_counter()
+        except Http404:
+            return TemplateResponse(
+                request, "vault/files_view.html", {"status": 404}, status=404
+            )
+        except MultipleObjectsReturned:
+            logger.error(f"multiple TreeNodes returned for {path}")
+            return TemplateResponse(
+                request, "vault/files_view.html", {"status": 404}, status=404
+            )
+    children = parent.children.all()
     return TemplateResponse(
         request,
         "vault/files_view.html",
