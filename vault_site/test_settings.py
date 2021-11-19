954f50d0a586c7a3ac86dca7a4216be5efa1f4fa
Alex Dempsey <avdempsey@archive.org>
2021-11-19 15:15:32 -0800

    Initial pip-tools, pytest and playwright config

---
+++ vault_site/test_settings.py
@@ -0,0 +1,17 @@
+from .settings import *
+
+
+TEST_FIXTURE_USER = "tester"
+os.environ["REMOTE_USER"] = TEST_FIXTURE_USER
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": conf.get("VAULT_TEST_POSTGRES_NAME", "vault"),
+        "USER": conf.get("VAULT_TEST_POSTGRES_USER", "vault"),
+        "PASSWORD": conf.get("VAULT_TEST_POSTGRES_PASSWORD", "vault"),
+        "HOST": conf.get("VAULT_TEST_POSTGRES_HOST", "127.0.0.1"),
+        "PORT": conf.get("VAULT_TEST_POSTGRES_PORT", "55432"),
+        "DISABLE_SERVER_SIDE_CURSORS": True,
+    }
+}
