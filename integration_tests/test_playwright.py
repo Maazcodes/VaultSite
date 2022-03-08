9b66d4018842b5e8c3e631b5219372cbdcecf58c
Alex Dempsey <avdempsey@archive.org>
2022-03-07 23:17:24 -0800

    Turn xfail back on for test_playwright
    Test environment may need to run `playwright install`.

--- integration_tests/test_playwright.py
+++ integration_tests/test_playwright.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 import pytest
 
 
-# @pytest.mark.xfail(reason="environment probably doesn't have a browser available")
+@pytest.mark.xfail(reason="environment probably doesn't have a browser available")
 def test_dashboard_is_working(super_user, live_server):
     with sync_playwright() as p:
         browser = p.chromium.launch()
